dd207e354967b4e9a2a76eece5d8598a553dcf65
ning2413 <82848395+ning2413@users.noreply.github.com>
2023-11-16 17:08:44 -0500

    client_server_test

---
+++ UIClient-Server-Test/client_server_test/client_server_test/client_server_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  client_server_testApp.swift
+//  client_server_test
+//
+//  Created by Jim Ning on 11/16/23.
+//
+
+import SwiftUI
+
+@main
+struct client_server_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
