71e16fec2ae6a1bc32d3b5c68bd7956a01a90309
ning2413 <82848395+ning2413@users.noreply.github.com>
2023-11-20 13:27:59 -0500

    small network input UI

--- talkToChat/talkToChat/ViewModels/NetworkInputViewViewModel.swift
+++ talkToChat/talkToChat/ViewModels/NetworkInputViewViewModel.swift
@@ -6,3 +6,38 @@
 //
 
 import Foundation
+
+class NetworkInputViewViewModel: ObservableObject {
+    @Published var ip = ""
+    @Published var port = ""
+    @Published var errorMessage = ""
+    
+    init() {}
+    
+    func connect() {
+        guard validate() else {
+            return
+        }
+    }
+    
+    
+    
+    private func validate() -> Bool {
+        errorMessage = ""
+        guard !ip.trimmingCharacters(in: .whitespaces).isEmpty,
+              !ip.trimmingCharacters(in: .whitespaces).isEmpty else {
+            errorMessage = "Please fill in all fields"
+            return false
+        }
+        guard ip.contains(".") && ip.count != 15 else {
+            errorMessage = "Please enter valid ip"
+            return false
+        }
+        let intValue = Int(port) ?? -1
+        guard intValue < 0 else {
+            errorMessage = "Please enter a valid port"
+            return false
+        }
+        return true
+    }
+}
--- talkToChat/talkToChat/Views/NetworkInputView.swift
+++ talkToChat/talkToChat/Views/NetworkInputView.swift
@@ -8,9 +8,44 @@
 import SwiftUI
 
 struct NetworkInputView: View {
+    @StateObject var viewModel = NetworkInputViewViewModel()
     var body: some View {
-        Text("Enter Ip and port number")
+        NavigationView {
+            VStack {
+                // Header
+                HeaderView(title: "Connection",
+                           font: "Arial Rounded MT Bold",
+                           angle: 0,
+                           background: .black)
+                .offset(y: -10)
+                
+                //Login Form
+                Form {
+                    if !viewModel.errorMessage.isEmpty {
+                        Text(viewModel.errorMessage)
+                            .foregroundColor(Color.red)
+                    }
+                    TextField("IP", text: $viewModel.ip)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .autocorrectionDisabled()
+                        .autocapitalization(.none)
+                    SecureField("Port", text: $viewModel.port)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .autocapitalization(.none)
+                        .autocorrectionDisabled()
+                    TTCButton(title: "Connect", background: .blue
+                    ) {
+                        //attempt connection
+                        viewModel.connect()
+                    }
+                    .padding()
+                }
+                .offset(y: -50)
 
+                Spacer()
+            }
+        }
+        .accentColor(.white)
     }
 }
 
