d2d1b391743ae959142ec6795fcb406b0e970f14
ning2413 <82848395+ning2413@users.noreply.github.com>
2023-11-24 19:52:51 -0600

    connect to network

--- talkToChat/talkToChat/ViewModels/NetworkInputViewViewModel.swift
+++ talkToChat/talkToChat/ViewModels/NetworkInputViewViewModel.swift
@@ -1,43 +1,85 @@
 //
-//  NetworkInputViewViewModel.swift
-//  talkToChat
+//  ContentViewViewModel.swift
+//  client_server_test
 //
-//  Created by Jim Ning on 11/14/23.
+//  Created by Jim Ning on 11/20/23.
 //
-
+import Network
 import Foundation
+import Security
 
 class NetworkInputViewViewModel: ObservableObject {
-    @Published var ip = ""
-    @Published var port = ""
-    @Published var errorMessage = ""
-    
-    init() {}
-    
-    func connect() {
-        guard validate() else {
+    private var connection: NWConnection?
+    @Published var isConnected: Bool = false
+    func startConnection(host: String, portString: String) {
+        guard let port = UInt16(portString) else {
+            print("Invalid port")
             return
         }
+
+        let parameters = NWParameters(tls: NWProtocolTLS.Options())
+        let tlsOptions = parameters.defaultProtocolStack.applicationProtocols.first as! NWProtocolTLS.Options
+                sec_protocol_options_set_verify_block(tlsOptions.securityProtocolOptions, { sec_protocol_metadata, sec_trust, sec_protocol_verify_complete in
+                    
+                    // Load the local certificate
+                    guard let localCertificate = self.loadCertificate(named: "certificate") else {
+                        sec_protocol_verify_complete(false)
+                        return
+                    }
+
+                    // Evaluate the server's trust and compare it with the local certificate
+                    let serverCertificatesMatch = self.evaluateServerTrust(sec_trust, against: localCertificate)
+                    sec_protocol_verify_complete(serverCertificatesMatch)
+
+                }, DispatchQueue.main)
+
+        connection = NWConnection(host: NWEndpoint.Host(host), port: NWEndpoint.Port(rawValue: port)!, using: parameters)
+
+        connection?.stateUpdateHandler = { state in
+            switch state {
+            case .ready:
+                print("Connected to \(host)")
+                DispatchQueue.main.async {
+                    self.isConnected = true
+                }
+            case .failed(let error):
+                print("Failed to connect: \(error)")
+            default:
+                print("state: \(state)")
+                break
+            }
+        }
+        connection?.start(queue: .global())
     }
-    
-    
-    
-    private func validate() -> Bool {
-        errorMessage = ""
-        guard !ip.trimmingCharacters(in: .whitespaces).isEmpty,
-              !ip.trimmingCharacters(in: .whitespaces).isEmpty else {
-            errorMessage = "Please fill in all fields"
-            return false
+    func evaluateServerTrust(_ serverTrust: sec_trust_t, against localCertificate: SecCertificate) -> Bool {
+        // Implement the logic to evaluate the server's trust object against your local certificate
+        // This might involve comparing the public keys, certificate data, etc.
+        return true // return true if the server's certificate matches the local certificate
+    }
+    func loadCertificate(named name: String) -> SecCertificate? {
+        guard let url = Bundle.main.url(forResource: name, withExtension: "cer"),
+              let data = try? Data(contentsOf: url) else {
+            return nil
         }
-        guard ip.contains(".") && ip.count != 15 else {
-            errorMessage = "Please enter valid ip"
-            return false
+
+        return SecCertificateCreateWithData(nil, data as CFData)
+    }
+    func sendMessage(_ message: String) {
+        guard let data = message.data(using: .utf8) else {
+            print("Error: Unable to encode message to Data")
+            return
         }
-        let intValue = Int(port) ?? -1
-        guard intValue < 0 else {
-            errorMessage = "Please enter a valid port"
-            return false
-        }
-        return true
+
+        connection?.send(content: data, completion: .contentProcessed({ error in
+            if let error = error {
+                print("Error: Message sending failed with error \(error)")
+                return
+            }
+            print("Message successfully sent")
+        }))
+    }
+
+    func stopConnection() {
+        connection?.cancel()
     }
 }
