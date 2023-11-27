d345d0f55c95f5d8c86452bd03597a956bac3cf6
Haoyu Zhang <344055357@qq.com>
2023-11-26 20:24:01 -0500

    folders and readme

--- server/src/header.hpp
+++ server/src/header.hpp
@@ -26,7 +26,7 @@
 using namespace std;
 
 // server global constant
-const string apiKey = "Authorization: Bearer sk-"; 
+const string apiKey = "Authorization: Bearer <Replace with Your API KEY>"; 
 const string instruction = "Default";
 const string text_request = "Content-Type: application/json";
 const float temperature =  0.5;
